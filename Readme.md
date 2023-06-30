4d7f4b77bca65605cc4e3d0ffc8fdc6e81fbdf52
mauriciorimes <mauriciorimes@gmail.com>
2023-06-30 07:52:19 -0300

    ajustes

--- Readme.md
+++ Readme.md
@@ -1,3 +1,3 @@
 Usando o Tinkercard para simular uma placa de arduino, para desenvolver um algoritmo usando C, que pisque em led a mesma frequência do famoso sinal de ajuda, o SOS.
 
-link para observar a placa que foi construída: https://www.tinkercad.com/things/6IRoO39enwRhttps://www.tinkercad.com/things/6IRoO39enwR
+Link para observar a placa que foi construída e seu funcionamento:https://www.tinkercad.com/things/6IRoO39enwRhttps://www.tinkercad.com/things/6IRoO39enwR
--- index.c
+++ index.c
@@ -1,46 +1,46 @@
 void setup()
 {
-  pinMode(2,OUTPUT);  
+  pinMode(2, OUTPUT);  
 }
 
 void loop()
 {
-  digitalWrite(2,1); //ligando o led
+  digitalWrite(2, 1); //ligando o led
   delay(600);
-  digitalWrite(2,0); //apagando o led
+  digitalWrite(2, 0); //apagando o led
   delay(600);
-  digitalWrite(2,1); //ligando o led
+  digitalWrite(2, 1); //ligando o led
   delay(600);
-  digitalWrite(2,0); //apagando o led
+  digitalWrite(2, 0); //apagando o led
   delay(600);
-  digitalWrite(2,1); //ligando o led
+  digitalWrite(2, 1); //ligando o led
   delay(600);
-  digitalWrite(2,0); //apagando o led
+  digitalWrite(2, 0); //apagando o led
   delay(600);
   
-  digitalWrite(2,1); //ligando o led
+  digitalWrite(2, 1); //ligando o led
   delay(1000);
-  digitalWrite(2,0); //apagando o led
+  digitalWrite(2, 0); //apagando o led
   delay(1000);
-  digitalWrite(2,1); //ligando o led
+  digitalWrite(2, 1); //ligando o led
   delay(1000);
-  digitalWrite(2,0); //apagando o led
+  digitalWrite(2, 0); //apagando o led
   delay(1000);
-  digitalWrite(2,1); //ligando o led
+  digitalWrite(2, 1); //ligando o led
   delay(1000);
-  digitalWrite(2,0); //apagando o led
+  digitalWrite(2, 0); //apagando o led
   delay(1000);
   
-  digitalWrite(2,1); //ligando o led
+  digitalWrite(2, 1); //ligando o led
   delay(600);
-  digitalWrite(2,0); //apagando o led
+  digitalWrite(2, 0); //apagando o led
   delay(600);
-  digitalWrite(2,1); //ligando o led
+  digitalWrite(2, 1); //ligando o led
   delay(600);
-  digitalWrite(2,0); //apagando o led
+  digitalWrite(2, 0); //apagando o led
   delay(600);
-  digitalWrite(2,1); //ligando o led
+  digitalWrite(2, 1); //ligando o led
   delay(600);
-  digitalWrite(2,0); //apagando o led
+  digitalWrite(2, 0); //apagando o led
   delay(600);  
 }
